7b47c6a74708828dbc1510c8f6bb22a391874570
Murod Karabekov <karabekovofficial@gmail.com>
2025-10-02 09:40:29 +0500

    create all session

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+python3 session/create_session.py
---
+++ session/create_session.py
@@ -0,0 +1,24 @@
+import os
+from telethon import TelegramClient
+from dotenv import load_dotenv
+
+# .env fayldan ma'lumotlarni yuklaymiz
+load_dotenv()
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+
+if not API_ID or not API_HASH:
+    raise ValueError("❌ API_ID yoki API_HASH .env faylda topilmadi!")
+
+# Sessiyani yaratish uchun client
+client = TelegramClient("session/prostreamer", API_ID, API_HASH)
+
+async def main():
+    print("📲 Telegram sessiya yaratish jarayoni boshlandi...")
+    await client.start()
+    print("✅ Sessiya muvaffaqiyatli yaratildi va 'session/homebazar.session' faylida saqlandi.")
+
+if __name__ == "__main__":
+    with client:
+        client.loop.run_until_complete(main())
